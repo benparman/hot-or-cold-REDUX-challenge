e925709e93593daa15b1d8435c0ca749b9fdd25d
Ben Parman <benparman@gmail.com>
2018-12-16 23:39:48 -0700

    Add actions.js, convert restartGame function to action and move to actions.js

--- src/components/game.js
+++ src/components/game.js
@@ -16,14 +16,14 @@ export default class Game extends React.Component {
     };
   }
 
-  restartGame() {
-    this.setState({
-      guesses: [],
-      feedback: 'Make your guess!',
-      auralStatus: '',
-      correctAnswer: Math.floor(Math.random() * 100) + 1
-    });
-  }
+  // restartGame() {
+  //   this.setState({
+  //     guesses: [],
+  //     feedback: 'Make your guess!',
+  //     auralStatus: '',
+  //     correctAnswer: Math.floor(Math.random() * 100) + 1
+  //   });
+  // }
 
   makeGuess(guess) {
     guess = parseInt(guess, 10);
